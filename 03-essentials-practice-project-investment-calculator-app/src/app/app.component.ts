c0cadf64f2af98043d55e153f37b34ae9456a6cd
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2024-12-26 16:11:47 +0200

    03-essentials - use signals, reset form after submission

--- 03-essentials-practice-project-investment-calculator-app/src/app/app.component.ts
+++ 03-essentials-practice-project-investment-calculator-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 
 import type { InvestmentInput } from './investment-input.model';
@@ -13,14 +13,14 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
     imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent]
 })
 export class AppComponent {
-  resultsData?: {
+  resultsData = signal<{
     year: number;
     interest: number;
     valueEndOfYear: number;
     annualInvestment: number;
     totalInterest: number;
     totalAmountInvested: number;
-  }[];
+  }[] | undefined>(undefined);
 
   onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
@@ -44,6 +44,6 @@ export class AppComponent {
       });
     }
 
-    this.resultsData = annualData;
+    this.resultsData.set(annualData);
   }
 }
