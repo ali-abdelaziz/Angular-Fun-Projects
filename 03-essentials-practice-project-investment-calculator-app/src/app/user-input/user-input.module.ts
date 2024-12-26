6025779a3077e3a26fdeb0fde34b5c5a2b559024
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2024-12-26 17:02:40 +0200

    03-essentials - use module based structures

--- 03-essentials-practice-project-investment-calculator-app/src/app/user-input/user-input.module.ts
+++ 03-essentials-practice-project-investment-calculator-app/src/app/user-input/user-input.module.ts
@@ -1,11 +1,11 @@
-// import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-// import { UserInputComponent } from './user-input.component';
+import { UserInputComponent } from './user-input.component';
 
-// @NgModule({
-//   declarations: [UserInputComponent],
-//   imports: [FormsModule],
-//   exports: [UserInputComponent]
-// })
-// export class UserInputModule {}
+@NgModule({
+  declarations: [UserInputComponent],
+  imports: [FormsModule],
+  exports: [UserInputComponent]
+})
+export class UserInputModule {}
