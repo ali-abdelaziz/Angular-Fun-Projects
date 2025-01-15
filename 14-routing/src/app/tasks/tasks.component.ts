2925469013fa7a4e96c2134eff6a3a7a05228d0c
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2025-01-15 15:19:51 +0200

    use query parameters for data manipulation [sort user tasks based on query parameters]

--- 14-routing/src/app/tasks/tasks.component.ts
+++ 14-routing/src/app/tasks/tasks.component.ts
@@ -5,6 +5,7 @@ import {
   computed,
   inject,
   input,
+  signal,
 } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 
@@ -21,17 +22,27 @@ import { TasksService } from './tasks.service';
 export class TasksComponent implements OnInit {
   userId = input.required<string>();
   // order = input<'asc' | 'desc'>();
-  order?: 'asc' | 'desc';
+  order = signal<'asc' | 'desc'>('desc');
   private tasksService = inject(TasksService);
   userTasks = computed(() =>
-    this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+    this.tasksService
+      .allTasks()
+      .filter((task) => task.userId === this.userId())
+      .sort((a, b) => {
+        // sort based on id
+        if (this.order() === 'desc') {
+          return a.id > b.id ? -1 : 1;
+        } else {
+          return a.id > b.id ? 1 : -1;
+        }
+      })
   );
   private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     const subscription = this.activatedRoute.queryParams.subscribe({
-      next: (params) => (this.order = params['order']),
+      next: (params) => this.order.set(params['order']),
     });
 
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
