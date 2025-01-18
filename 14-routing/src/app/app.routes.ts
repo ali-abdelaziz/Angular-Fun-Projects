e3c19329739036fae4cd1545135a900d242d13a5
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2025-01-18 14:32:37 +0200

    set, resolve titles

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -2,14 +2,15 @@ import { Routes } from '@angular/router';
 
 import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { resolveTitle, resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { Title } from '@angular/platform-browser';
 
 export const routes: Routes = [
   {
     path: '', // <your-domain>/
     component: NoTaskComponent,
-
+    title: 'No Task Selected',
     // redirectTo: '/users/u1',
     // pathMatch: 'prefix' // detects an error as every path is starting with ''
     // pathMatch: 'full'
@@ -24,7 +25,8 @@ export const routes: Routes = [
     },
     resolve: {
       userName: resolveUserName,
-    }
+    },
+    title: resolveTitle,
   },
   {
     path: '**', // <your-domain>/<any-other-path>
